514c96ae48046eb29ebf25035c79fbf6e9ee4b1f
Anthony Pangelinan <Anthony Pangelinan>
2017-03-23 23:14:16 -0700

    Add Matrix.java

--- Matrix.java
+++ Matrix.java
@@ -1,28 +1,58 @@
+/*
+This was created independently by, and is the intellectual property of, Anthony Pangelinan.
+
+This is a Matrix object that stores information regarding an n by m matrix of doubles.
+This object keeps track of how many rows and columns are in the matrix, and has basic functions 
+   to return information about the Matrix Object.
+*/
+
 public class Matrix {
    private int rows;
    private int columns;
-   private double[][] myMatrix;
+   private double[][] myMatrix; // nxm matrix is stored
    
+   // This initializes the matrix to the desired size.
+   // Matrix takes integers of the desired number of rows and columns as parameters.
    public Matrix(int rows, int columns) {
       this.rows = rows;
       this.columns = columns;
       myMatrix = new double[rows][columns];
    }
    
+   // This returns an integer of the number of rows in the matrix (the value of n).
    public int getRows() {
       return rows;
    }
    
+   // This returns an integer of the number of columns in the matrix (the value of m).
    public int getColumns() {
       return columns;
    }
    
+   // This edits the contents of the matrix at the desired location.
+   // This function alerts the user if the desired matrix location does not exist for
+   //    this matrix.
+   // add takes integers of the desired row and column to edit, and a double of the value
+   //    to be added to the matrix, as parameters.
    public void add(int row, int column, double value) {
-      myMatrix[row][column] = value;
+      if(row < 0 || column < 0 || row > this.rows || column > this.columns) {
+         System.out.println("Desired location outside of matrix boundaries.");
+      } else {
+         myMatrix[row][column] = value;
+      }
    }
    
+   // This gets the contents of the matrix at the desired location.
+   // This function alerts the user if the desired matrix location does not exist for
+   //    this matrix.
+   // getValue takes integers of the desired row and column as parameters.
    public double getValue(int row, int column) {
-      return myMatrix[row][column];
+      if(row < 0 || column < 0 || row > this.rows || column > this.columns) {
+         System.out.println("Desired location outside of matrix boundaries.");
+         return -1;
+      } else {
+         return myMatrix[row][column];
+      }
    }
    
    // This prints the full matrix to the console.
@@ -37,7 +67,7 @@ public class Matrix {
                System.out.print("    " + myMatrix[row][column] + "   ");
             } else {
                System.out.print("   " + myMatrix[row][column] + "   ");
-            }   
+            }
          }
          System.out.println("|");
       }
